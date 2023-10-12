65b7f44fffa636543d04ae803cb2439350c39e75
Marudha-Nayagam <marudhanayagam98@gmail.com>
2023-10-12 18:12:14 +0530

    updated api

--- src/global.js
+++ src/global.js
@@ -1,2 +1,3 @@
 //export const API = "https://b44-product-app.onrender.com";
-export const API = "http://localhost:7000";
+//export const API = "http://localhost:7000";
+export const API = "https://product-backend-ger0.onrender.com";
